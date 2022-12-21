62ed47c5c4e5c104100fe253cef003f4b0c1add6
calvin d. woo <calvin.d.woo@gmail.com>
2022-12-21 16:24:28 -0500

    s4.2.3: add container class for a token

---
+++ pylox/token.py
@@ -0,0 +1,18 @@
+from pylox.token_type import TokenType
+
+
+class Token:
+    def __init__(
+        self,
+        type: TokenType,
+        lexeme: str,
+        literal: object,
+        line: int
+    ):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def to_string(self) -> str:
+        return f"{self.type} {self.lexeme} {self.literal}"
